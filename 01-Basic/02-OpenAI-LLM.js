3501fc2e05e371c8be6aff05b5056bf62d295695
지승 <rjs8833@gmail.com>
2025-07-21 13:46:04 +0900

    stream example

--- 01-Basic/02-OpenAI-LLM.js
+++ 01-Basic/02-OpenAI-LLM.js
@@ -11,6 +11,15 @@ const llm = new ChatOpenAI({
 	model: 'gpt-3.5-turbo',
 })
 
-const response = await llm.invoke('대한민국의 수도는 어디인가요?')
+// const response = await llm.invoke('대한민국의 수도는 어디인가요?')
 
-console.log('답변:', response)
+// console.log('답변:', response)
+
+const result = await llm.stream('대한민국의 아름다운 관광지 10곳과 주소를 알려주세요!')
+const chunks = []
+for await (const chunk of result) {
+	chunks.push(chunk.content)
+	console.log(`${chunk.content}|`)
+}
+
+console.log(chunks.join(''))
